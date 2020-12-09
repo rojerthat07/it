653786cd0f00e5d2a694510950a7053f6effe33d
Jer Carlo Catallo <jercarlopogi07@gmail.com>
2020-12-09 09:50:12 +0800

    Updates

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-$(function () {
-  const $gallery = $(".gallery a").simpleLightbox();
-});
+document.getElementById("main-title").innerHTML = "Barangay Online Transaction";
+
+function contactus() {
+  alert("You have been successfully sent an email");
+  e.preventDefault();
+}
+
+function submitrequest() {
+  alert("You have been successfully sent an request");
+  e.preventDefault();
+}
